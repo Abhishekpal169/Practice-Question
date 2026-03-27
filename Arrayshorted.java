3ca6766489d5c82fc9a8ad4308ca55b6ffd7ae52
Abhishek Pal <abhishekpal2437@gmail.com>
2026-03-27 22:14:36 +0530

    three

---
+++ Arrayshorted.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Arrayshorted {
+    public static void main(String[] args) {
+        int arr_1[] ={12,10,11,18,9};
+        String arr_2[]={"Abhishek","Sumit","Rohit","Kapil","Tusher","Nitesh","Deepak","Aayush"};
+        Arrays.sort(arr_1);
+        System.out.println("Arrays is short Order :"+Arrays.toString(arr_1));
+        Arrays.sort(arr_2);
+        System.out.println("Arrays is String Order :"+Arrays.toString(arr_2));
+
+        
+    }
+}
